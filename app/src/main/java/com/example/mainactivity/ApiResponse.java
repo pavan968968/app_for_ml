6f06be46daf372f248e479128e56623ecd1a66aa
pavan968968 <vvnspavan@gmail.com>
2025-02-16 14:43:49 +0530

    Fixed camera ratio issue

---
+++ app/src/main/java/com/example/mainactivity/ApiResponse.java
@@ -0,0 +1,9 @@
+package com.example.mainactivity;
+
+public class ApiResponse {
+    private String prediction;
+
+    public String getPrediction() {
+        return prediction;
+    }
+}
---
+++ app/src/main/java/com/example/mainactivity/ImageRequest.java
@@ -0,0 +1,9 @@
+package com.example.mainactivity;
+
+public class ImageRequest {
+    private String image; // Base64-encoded image
+
+    public ImageRequest(String image) {
+        this.image = image;
+}
+}
